745f4e50150aabfcca0b5d857eaac0fd84abaadb
laolvue <nrauglao23@gmail.com>
2020-06-12 01:04:30 -0500

    added materialbottomtabnavigator & vector icon dependencies

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
 const HomeScreen = ({navigation}) => {
   return (
     <View style={styles.default}>
         <Text>Profile Screen</Text>
+        <Icon name="checkbox-multiple-marked-circle-outline" size={30} color="#4F8EF7" />
         <Button
             title = 'Go to Home'
             onPress = {() => {navigation.navigate('Home')}}
